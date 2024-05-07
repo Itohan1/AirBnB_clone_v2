b42b7ebc19dfae278361cb2bf1adc6401bd6cf50
Itohan <momoduitohan5@gmail.com>
2024-05-07 12:05:42 +0100

    these permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,20 +2,12 @@
 # Write a Bash script that sets up your web servers for the deployment
 
 nginx_path="/etc/nginx"
-data_folder="/data"
-data_static="$data_folder/web_static"
-data_releases="$data_static/releases"
-#data_shared="$data_static/shared"
-#data_test="$data_releases/test"
-fake_html="$data_test/index.html"
-data_current="$data_static/current"
-
 if [ ! -e "$nginx_path" ]; then
         sudo apt-get update
         sudo apt-get install -y nginx
 fi
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo chown -R ubuntu:ubuntu "$data_folder"
+sudo chown -R ubuntu:ubuntu /data
 html_content='<html>
         <head>
         </head>
@@ -23,10 +15,10 @@ html_content='<html>
                 Holberton School
         </body>
 </html>'
-echo "$html_content" | sudo tee "$fake_html"
-sudo ln -sf "$data_test" "$data_current"
+echo "$html_content" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo sed -i '/server_name_;/a \\tlocation /hbnb_static/ {\n\t\t alias '"$data_current"';\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name_;/a \\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
 
