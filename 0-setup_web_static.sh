bf50639fa4654bc930ebb0fc07e6e0274e5d3737
Itohan <momoduitohan5@gmail.com>
2024-05-07 12:16:22 +0100

    these permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,12 +6,11 @@ if ! [ -x "$(command -v nginx)" ]; then
         sudo apt-get update
         sudo apt-get install -y nginx
 fi
-sudo service nginx restart
+sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo chown -R ubuntu:ubuntu /data
 echo "<html><head></head><body> Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/server_name_;/a \\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
