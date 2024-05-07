44af253d0d9dcdb96ec3ef24de4fbf35b6c26a24
Itohan <momoduitohan5@gmail.com>
2024-05-07 12:41:56 +0100

    these permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,15 +2,15 @@
 # Write a Bash script that sets up your web servers for the deployment
 
 if ! [ -x "$(command -v nginx)" ]; then
-        sudo apt-get update
-        sudo apt-get install -y nginx
+	sudo apt-get update
+	sudo apt-get install -y nginx
 fi
 sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name_;/a \n\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name_;/a \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
 
