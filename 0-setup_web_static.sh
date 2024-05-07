3bbe776ceac3fea0595f2c90a646dd3c06a25524
Itohan <momoduitohan5@gmail.com>
2024-05-07 12:46:34 +0100

    these permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name_;/a \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i "/^\s*server_name\s/ a \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 
 sudo service nginx restart
 
